47013f5432b02b063413e16dfd3dfd703dbac3af
Jannes Höke <mail@jhoeke.de>
2025-05-25 16:57:19 +0200

    ♻️ Move transcription cache to single data object in preparation of adding a settings panel

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,12 +1,16 @@
 import { bdApi } from './shared';
 
+const CACHE_KEY = `transcriptionCache`;
+
 export const loadTranscription = (itemId: string) => {
-  const transcription = bdApi.Data.load(`transcription-${itemId}`);
-  if (transcription) {
-    return transcription as string;
+  const transcriptionCache = bdApi.Data.load(CACHE_KEY);
+  if (transcriptionCache) {
+    return transcriptionCache[itemId] as string;
   }
 };
 
 export const saveTranscription = (itemId: string, transcription: string) => {
-  bdApi.Data.save(`transcription-${itemId}`, transcription);
+  const transcriptionCache = bdApi.Data.load(CACHE_KEY) ?? {};
+  transcriptionCache[itemId] = transcription;
+  bdApi.Data.save(CACHE_KEY, transcriptionCache);
 };
