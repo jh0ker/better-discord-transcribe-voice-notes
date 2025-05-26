2592c9ae915fc5f0cb1161bc72b71fbed36d047d
Jannes Höke <mail@jhoeke.de>
2025-05-26 20:10:41 +0200

    ♻️ Move transcription logic into lib

--- src/components/transcribe-button.tsx
+++ src/components/transcribe-button.tsx
@@ -1,15 +1,7 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 
-import {
-  loadSettings,
-  loadTranscription,
-  saveTranscription,
-} from '../lib/data';
 import { buttonStyle } from '../lib/style';
-import {
-  OPENAI_DEFAULT_BASE_URL,
-  OPENAI_DEFAULT_MODEL,
-} from '../lib/transcription';
+import { useTranscription } from '../lib/transcription';
 
 type OriginalItem = {
   content_scan_version: Number;
@@ -44,61 +36,8 @@ const TranscribeButton: React.FC<TranscribeButtonProps> = ({ item }) => {
     return <React.Fragment></React.Fragment>;
   }
 
-  const [transcription, setTranscription] = useState(
-    loadTranscription(item.uniqueId)
-  );
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [failureReason, setFailureReason] = useState<unknown | undefined>();
-
-  const transcribeItem = useCallback(async () => {
-    console.log('Transcribing item', item);
-    const settings = loadSettings();
-
-    if (settings.api.baseUrl === undefined && settings.api.token === '') {
-      BdApi.UI.alert(
-        'Invalid configuration',
-        'Please configure your API access in the plugin settings.'
-      );
-      return;
-    }
-
-    try {
-      setIsLoading(true);
-
-      // Download the file
-      const voiceNoteResponse = await fetch(item.originalItem.proxy_url);
-      const voiceNoteBlob = await voiceNoteResponse.blob();
-
-      // Run the transcription with the currently configured settings
-      const formData = new FormData();
-      formData.append('file', voiceNoteBlob, item.originalItem.filename);
-      formData.append('model', settings.api.model ?? OPENAI_DEFAULT_MODEL);
-
-      const baseURL = settings.api.baseUrl ?? OPENAI_DEFAULT_BASE_URL;
-      const openaiResponse = await fetch(`${baseURL}/audio/transcriptions`, {
-        method: 'POST',
-        body: formData,
-        headers: {
-          Authorization: `Bearer ${settings.api.token}`,
-        },
-      });
-      const openaiResponseJson = await openaiResponse.json();
-      if (!openaiResponse.ok) {
-        throw openaiResponseJson;
-      }
-
-      saveTranscription(item.uniqueId, openaiResponseJson.text);
-      setTranscription(openaiResponseJson.text);
-      setIsLoading(false);
-      setIsError(false);
-    } catch (e) {
-      setIsLoading(false);
-      setIsError(true);
-      setFailureReason(e);
-      console.error(e);
-    }
-  }, [item]);
+  const { transcription, isLoading, isError, failureReason, transcribeItem } =
+    useTranscription(item);
 
   return (
     <div
--- src/lib/transcription.ts
+++ src/lib/transcription.ts
@@ -1,2 +1,72 @@
+import { useCallback, useState } from 'react';
+
+import { loadSettings, loadTranscription, saveTranscription } from './data';
+import type { Item } from '../components/transcribe-button';
+
 export const OPENAI_DEFAULT_BASE_URL = 'https://api.openai.com/v1';
 export const OPENAI_DEFAULT_MODEL = 'whisper-1';
+
+export const useTranscription = (item: Item) => {
+  const [transcription, setTranscription] = useState(
+    loadTranscription(item.uniqueId)
+  );
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [failureReason, setFailureReason] = useState<unknown | undefined>();
+
+  const transcribeItem = useCallback(async () => {
+    console.log('Transcribing item', item);
+    const settings = loadSettings();
+
+    if (settings.api.baseUrl === undefined && settings.api.token === '') {
+      BdApi.UI.alert(
+        'Invalid configuration',
+        'Please configure your API access in the plugin settings.'
+      );
+      return;
+    }
+
+    try {
+      setIsLoading(true);
+
+      // Download the file
+      const voiceNoteResponse = await fetch(item.originalItem.proxy_url);
+      const voiceNoteBlob = await voiceNoteResponse.blob();
+
+      // Run the transcription with the currently configured settings
+      const formData = new FormData();
+      formData.append('file', voiceNoteBlob, item.originalItem.filename);
+      formData.append('model', settings.api.model ?? OPENAI_DEFAULT_MODEL);
+
+      const baseURL = settings.api.baseUrl ?? OPENAI_DEFAULT_BASE_URL;
+      const openaiResponse = await fetch(`${baseURL}/audio/transcriptions`, {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${settings.api.token}`,
+        },
+      });
+      const openaiResponseJson = await openaiResponse.json();
+      if (!openaiResponse.ok) {
+        throw openaiResponseJson;
+      }
+
+      saveTranscription(item.uniqueId, openaiResponseJson.text);
+      setTranscription(openaiResponseJson.text);
+      setIsLoading(false);
+      setIsError(false);
+    } catch (e) {
+      setIsLoading(false);
+      setIsError(true);
+      setFailureReason(e);
+      console.error(e);
+    }
+  }, [item]);
+  return {
+    transcription,
+    isLoading,
+    isError,
+    failureReason,
+    transcribeItem,
+  };
+};
